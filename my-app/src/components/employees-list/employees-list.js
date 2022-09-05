4c1630aed49027f926daa51a05aeac4146da80c6
Vladislav Vlasenko <you@example.comvlad-vlasenko@mail.ru>
2022-09-05 14:38:04 +0300

    создание каркаса

---
+++ my-app/src/components/employees-list/employees-list.js
@@ -0,0 +1,15 @@
+import React from "react";
+import EmployeesListItem from "../employees-list-item/employees-list-item";
+import './employees-list.css';
+
+const EmployeesList = () => {
+    return (
+        <ul className="app-list list-group">
+            <EmployeesListItem />
+            <EmployeesListItem />
+            <EmployeesListItem />
+        </ul>
+    );
+}
+
+export default EmployeesList;
